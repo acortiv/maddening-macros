19e9615d813b82b5027f688254db293678c552ae
acortiv <tacortiv@proton.me>
2026-02-01 16:44:34 -0700

    fix: macro challenge #2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-// use maddening_macros::mm_assert_ne;
-
-use maddening_macros::{FoldToDisplay, fold_to_display_for};
-
 fn main() {
     println!("Hello, world!");
     // ************************************************
@@ -9,8 +5,4 @@ fn main() {
     // mm_assert_ne!(3, 3);
     // assert_ne!(3, 3);
     // ************************************************
-    let a = "Hello";
-    let b = String::from(" World! ");
-    let c = 1312026;
-    println!(fold_to_display_for!(a, b, c));
 }
