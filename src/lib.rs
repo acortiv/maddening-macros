bdc34233389b95240a404a7028797d06f83a186e
acortiv <tacortiv@proton.me>
2026-01-31 21:40:45 -0700

    fix: don't know what is wrong

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,35 @@ macro_rules! mm_assert_ne {
         }
     };
 }
+
+pub trait FoldToDisplay {
+    fn fold_to_display(&self) -> String;
+}
+
+#[macro_export]
+macro_rules! fold_to_display_for {
+    ($last:ident) => {
+        impl<$last: Display> FoldToDisplay for ($last,) {
+            fn fold_to_display(&self) -> String {
+                let (t,) = self;
+                t.to_string()
+            }
+        }
+    };
+    ($first:ident , $($rest:ident),+) => {
+        $crate::fold_to_display_for!($($rest),+);
+
+        impl<$first, $($rest,)* D: Display> FoldToDisplay for ($first, $($rest,)* D ) {
+            fn fold_to_display(&self) -> String {
+                let ($first, $($rest,)* t) = self;
+                let mut acc = String::new();
+                acc.push_str(&$first.to_string());
+                $(
+                    acc.push_str(&$rest.to_string());
+                )*
+                acc.push_str(&t.to_string());
+                acc
+        }
+        }
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-use maddening_macros::mm_assert_ne;
+// use maddening_macros::mm_assert_ne;
+
+use maddening_macros::{FoldToDisplay, fold_to_display_for};
 
 fn main() {
     println!("Hello, world!");
-    mm_assert_ne!(3, 4);
+    // ************************************************
+    // # A quick custom implementation of assert_ne
+    // mm_assert_ne!(3, 3);
+    // assert_ne!(3, 3);
+    // ************************************************
+    let a = "Hello";
+    let b = String::from(" World! ");
+    let c = 1312026;
+    println!(fold_to_display_for!(a, b, c));
 }
